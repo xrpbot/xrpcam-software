8510ca6c19a33c29d46ca8fc05ef9da191ff0018
Norbert Braun <norbert@xrpbot.org>
2021-01-23 19:23:57 +0100

    gateware: byte swap generated bitstream

--- gateware/zedboard.py
+++ gateware/zedboard.py
@@ -14,3 +14,17 @@ class ZedboardPlatform(Xilinx7SeriesPlatform):
         *LEDResources(pins="T22 T21 U22 U21 V22 W22 U19 U14", attrs=Attrs(IOSTANDARD="LVCMOS33"))
     ]
     connectors = []
+
+    def toolchain_prepare(self, fragment, name, **kwargs):
+        # The Zynq driver in the FPGA Manager framework on mainline Linux
+        # expects bitstreams that are byte swapped with respect to what the
+        # Vivado command `write_bitstream -bin_file` produces. Thus, use the
+        # `write_cfgmem` command with appropriate options to generate the
+        # bitstream (.bin file).
+        overrides = {
+            "script_after_bitstream":
+                "write_cfgmem -force -format bin -interface smapx32 -disablebitswap "
+                "-loadbit \"up 0 {name}.bit\" {name}.bin".format(name=name)
+        }
+
+        return super().toolchain_prepare(fragment, name, **overrides, **kwargs)
